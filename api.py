8e0785a7dea617049f55fd5e4c6f58f0b1bf43ca
TerrorBhai <152867529+vickyhubhai@users.noreply.github.com>
2025-10-30 15:23:43 +0530

    Add files via upload

--- api.py
+++ api.py
@@ -84,4 +84,5 @@ def api_provider(provider):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    debug = os.environ.get('FLASK_DEBUG', 'False').lower() == 'true'
+    app.run(host='0.0.0.0', port=port, debug=debug)
